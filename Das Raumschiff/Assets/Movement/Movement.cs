2b2c42a3cd4e786fa9f64ba943961e44fd362c1c
Tobias <rohrer-tobias@t-online.de>
2023-04-12 18:06:31 +0200

    start

---
+++ Das Raumschiff/Assets/Movement/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f; // The speed of the movement
+    private float verticalInput; // Input for vertical movemen
+    public Rigidbody2D b;
+
+    void Awake(){
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+void Update()
+    {
+        // Get input for vertical movement
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Move the GameObject up or down based on the input
+        transform.Translate(Vector3.up * speed * Time.deltaTime * verticalInput);
+    }
+}
