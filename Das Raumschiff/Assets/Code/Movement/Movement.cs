85128b6cbf1d1edf9722fb34404c6c7b7510b40d
Tobias <rohrer-tobias@t-online.de>
2023-05-05 12:38:20 +0200

    start

--- Das Raumschiff/Assets/Code/Movement/Movement.cs
+++ Das Raumschiff/Assets/Code/Movement/Movement.cs
@@ -1,24 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
+    public GameObject Bullet;
     public float speed; // The speed of the movement
     private float verticalInput; // Input for vertical movemen
     public Stats stats;
+    public float ti;
+    public float atkcooldwn = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        stats = GetComponent<Stats>();
-        speed = stats.speed;
+
     }
 
     // Update is called once per frame
 void Update()
     {
         movement();
+        shoot();
     }
     
     public void movement(){
@@ -26,6 +31,18 @@ void Update()
         verticalInput = Input.GetAxis("Vertical");
 
         // Move the GameObject up or down based on the input
-        transform.Translate(Vector3.right* speed * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.left* speed * Time.deltaTime * verticalInput);
+        if(transform.position.y>=2.5||transform.position.y<=-2.5){
+            SceneManager.LoadScene("Game_Over");
+        }
+    }
+    public void shoot(){
+        if(Input.GetKey(KeyCode.Space)){
+            if(ti>atkcooldwn){
+                Instantiate(Bullet,transform.position,Quaternion.identity);
+                ti=0;
+            }
+        }
+        ti=ti+10*Time.deltaTime;
     }
 }
--- Das Raumschiff/Assets/Code/Movement/Raumschiff.cs
+++ Das Raumschiff/Assets/Code/Movement/Raumschiff.cs
@@ -1,19 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Raumschiff : MonoBehaviour
 {
+    public GameObject Bullet;
+    public float x;
+    public float y;
+    public float z;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shoot();
     }
-    
+
+    public void shoot(){
+        if(Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(Bullet,transform.position);
+        }
+    }
+
+    private void Instantiate(GameObject bullet, Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
 }
